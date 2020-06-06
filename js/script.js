179618c291e66052f64c28b333ec7c297b0a35c1
Jonas Johansson <jns.johansson@gmail.com>
2020-06-06 14:32:44 +0200

    final content added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 window.onload = () => {
-  shuffle(worlds);
-  shuffle(letters3d);
-  shuffle(designofdata);
-  shuffle(hommage);
-  shuffle(letters);
-
-  var mrObjects = worlds.concat(letters3d);
-  addObjects(mrObjects, 'mr');
-  addObjects(designofdata, 'gdod');
-  var gdObjects = hommage.concat(letters);
-  addObjects(gdObjects, 'gd');
+  shuffle(dod1);
+  shuffle(dod2);
+  shuffle(dod3);
+  shuffle(hnw1);
+  shuffle(hnw2);
+  shuffle(hnw3);
+  addObjects(hnw3, 'hnw3');
+  addObjects(dod3, 'dod3');
+  addObjects(hnw2, 'hnw2');
+  addObjects(dod2, 'dod2');
+  addObjects(hnw1, 'hnw1');
+  addObjects(dod1, 'dod1');
 };
 
 function addObjects(objects, folder) {
@@ -34,21 +35,7 @@ function addObjects(objects, folder) {
   //item.parentNode.removeChild(item);
 }
 
-var hommage = [
-  ['https://editor.p5js.org/Emelie/sketches/xtae9qLuR', 'yasoi.jpg'],
-  ['https://editor.p5js.org/thornvik/sketches/c2J8wmEXc', 'rolling.gif'],
-  ['https://editor.p5js.org/strazan/sketches/veAPSc340', 'mondrian.gif'],
-  ['https://editor.p5js.org/rockon_d/sketches/VBBl-h_Ap', 'triangles.png'],
-  ['https://editor.p5js.org/augusterixon/sketches/AwmvTqIep', 'warhol.gif'],
-  ['https://editor.p5js.org/Empi/sketches/3XmBmEH_f', 'triangles2.png'],
-  ['https://editor.p5js.org/douglaslind/sketches/LfWQXCFb0', 'checker.png'],
-  ['https://editor.p5js.org/MuchChinz/sketches/GvbvfHXYI', 'squares.gif'],
-  ['https://editor.p5js.org/asab/sketches/89xSwbBIX', 'arc.gif'],
-  ['https://editor.p5js.org/fannynilsson/sketches/vk7yZzEuC', 'hilma.png'],
-  ['https://editor.p5js.org/pjhochman/sketches/tEbPFwlNp', 'jazz.gif']
-];
-
-var letters = [
+var dod1 = [
   ['https://editor.p5js.org/emiliohell/sketches/voorOMpzL', 'e.gif'],
   ['https://editor.p5js.org/Anderskillander/sketches/DV1Ojf6R0', 'a.gif'],
   ['https://editor.p5js.org/Mudassar/sketches/A-gtgSc1R', 'm.gif'],
@@ -67,7 +54,39 @@ var letters = [
   ['https://editor.p5js.org/MuchChinz/sketches/fUhmhMH8I', 'x2.gif']
 ];
 
-var letters3d = [
+var dod2 = [
+  ['https://editor.p5js.org/Emelie/sketches/xtae9qLuR', 'yasoi.jpg'],
+  ['https://editor.p5js.org/thornvik/sketches/c2J8wmEXc', 'rolling.gif'],
+  ['https://editor.p5js.org/strazan/sketches/veAPSc340', 'mondrian.gif'],
+  ['https://editor.p5js.org/rockon_d/sketches/VBBl-h_Ap', 'triangles.png'],
+  ['https://editor.p5js.org/augusterixon/sketches/AwmvTqIep', 'warhol.gif'],
+  ['https://editor.p5js.org/Empi/sketches/3XmBmEH_f', 'triangles2.png'],
+  ['https://editor.p5js.org/douglaslind/sketches/LfWQXCFb0', 'checker.png'],
+  ['https://editor.p5js.org/MuchChinz/sketches/GvbvfHXYI', 'squares.gif'],
+  ['https://editor.p5js.org/asab/sketches/89xSwbBIX', 'arc.gif'],
+  ['https://editor.p5js.org/fannynilsson/sketches/vk7yZzEuC', 'hilma.png'],
+  ['https://editor.p5js.org/pjhochman/sketches/tEbPFwlNp', 'jazz.gif']
+];
+
+var dod3 = [
+  ['https://rtro-wthr.netlify.app/', 'weatherspirals.gif'],
+  ['https://generativecolorart.netlify.app/', 'artgrad.gif'],
+  ['https://editor.p5js.org/rainnie/present/pq19a97cy', 'birdcage.gif'],
+  ['https://editor.p5js.org/MuchChinz/full/CrRizZexD', 'bullshit.gif'],
+  ['https://peterscolorpalette.netlify.app/', 'gradient.gif'],
+  ['https://variable-birdsong.netlify.app/', 'koltrast.gif'],
+  ['https://editor.p5js.org/thornvik/present/-YmbK3S3D', 'lines.gif'],
+  ['https://editor.p5js.org/joakimhellgren/full/a1HeJ8gHH', 'musicdots.gif'],
+  ['https://priceless-bardeen-5c875c.netlify.app/', 'particles.gif'],
+  ['https://editor.p5js.org/TQM/present/DrmQJ0R2A', 'smileys.gif'],
+  ['https://dgozen-quotey.glitch.me/', 'quotey.jpg'],
+  ['https://wealthvshappiness.netlify.app/', 'wealthspiral.gif'],
+  ['https://editor.p5js.org/tim.olsson/present/5mlRyUuw-', 'whale.gif'],
+  ['https://discus.netlify.app/', 'musicspiral.jpg'],
+  ['https://robotquotes.netlify.app/', 'ai.jpg']
+];
+
+var hnw1 = [
   ['https://a-frame-k-letter.glitch.me/', 'k.gif'],
   ['https://courageous-meteorology.glitch.me/', 't.gif'],
   ['https://coherent-palm-clef.glitch.me/', 'g.gif'],
@@ -84,7 +103,7 @@ var letters3d = [
   ['https://concrete-phase-leopard.glitch.me/', 'n.gif']
 ];
 
-var worlds = [
+var hnw2 = [
   ['https://zcream.glitch.me/', 'scream.gif'],
   ['https://economic-mahogany-frigate.glitch.me/', 'trumpet.gif'],
   ['https://lime-handy-butterfly.glitch.me/', 'space.gif'],
@@ -93,22 +112,15 @@ var worlds = [
   ['https://amenable-jasper-smelt.glitch.me/', 'knee.gif']
 ];
 
-var designofdata = [
-  ['https://rtro-wthr.netlify.app/', 'weatherspirals.gif'],
-  ['https://generativecolorart.netlify.app/', 'artgrad.gif'],
-  ['https://editor.p5js.org/rainnie/present/pq19a97cy', 'birdcage.gif'],
-  ['https://editor.p5js.org/MuchChinz/full/CrRizZexD', 'bullshit.gif'],
-  ['https://peterscolorpalette.netlify.app/', 'gradient.gif'],
-  ['https://variable-birdsong.netlify.app/', 'koltrast.gif'],
-  ['https://editor.p5js.org/thornvik/present/-YmbK3S3D', 'lines.gif'],
-  ['https://editor.p5js.org/joakimhellgren/full/a1HeJ8gHH', 'musicdots.gif'],
-  ['https://priceless-bardeen-5c875c.netlify.app/', 'particles.gif'],
-  ['https://editor.p5js.org/TQM/present/DrmQJ0R2A', 'smileys.gif'],
-  ['https://dgozen-quotey.glitch.me/', 'quotey.jpg'],
-  ['https://wealthvshappiness.netlify.app/', 'wealthspiral.gif'],
-  ['https://editor.p5js.org/tim.olsson/present/5mlRyUuw-', 'whale.gif'],
-  ['https://discus.netlify.app/', 'musicspiral.jpg'],
-  ['https://robotquotes.netlify.app/', 'ai.jpg']
+var hnw3 = [
+  ['https://borgor.netlify.app/', 'borgor.gif'],
+  ['https://emelieedman.github.io/vr-experience/', 'desert.gif'],
+  ['https://minifishies.netlify.app/', 'fishies.gif'],
+  ['https://vigorous-poitras-ae86e3.netlify.app/', 'frahm.gif'],
+  ['https://vr-artsyroom.glitch.me/', 'gallery.gif'],
+  ['https://a-frame-explore.glitch.me/', 'poc.gif'],
+  ['https://a-frame-gallery.now.sh/', 'poem.gif'],
+  ['https://project11.netlify.app/', 'shivering.gif']
 ];
 
 function shuffle(array) {
